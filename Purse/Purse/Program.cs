ccc6e7fdf0b6c6586ccd79de3ed09e98813d0f4f
SergiyKolochun <Sergiy.Kolochun@gmail.com>
2017-03-15 13:40:49 +0200

    adding BusinessCard

--- Purse/Purse/Program.cs
+++ Purse/Purse/Program.cs
@@ -10,6 +10,8 @@ namespace Purse
             Console.WriteLine(money.GetMoney());
             var credit = new CreditCard.CreditCard(500);
             Console.WriteLine(credit.GetSumCreditCard());
+            var bcard = new BusinessCard.BusinessCard("Iowlit");
+            Console.WriteLine(bcard.FindOutWhose());
         }
     }
 }
